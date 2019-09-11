e0ebc6e5080f679cc3f5a45a1fe9b124b97bab18
gilsonDias92 <gilsinho.1992@gmail.com>
2019-09-10 21:02:50 -0300

    Implementacao do metodo deletar_tarefa, funcionando corretamente

--- ListaDeTarefas/gerenciador_de_tarefas/app/urls.py
+++ ListaDeTarefas/gerenciador_de_tarefas/app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('listar_tarefas/', listar_tarefas, name='listar_tarefas'),
     path('cadastrar_tarefa/', cadastrar_tarefa, name='cadastrar_tarefa'),
     path('editar_tarefa/<int:id>', editar_tarefa, name='editar_tarefa'),
+    path('remover_tarefa/<int:id>', remover_tarefa, name='remover_tarefa'),
     # entre <> passamos o tipo do parametro e o parametro, deve ser o mesmo nome no metodo editar_tarefa na view
 ]
